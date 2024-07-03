59b1b924eec25302b6bcf1272feb710ff30f262b
Chu-duong <demonss0910@gmail.com>
2024-07-03 11:32:07 +0700

    add navigation bar

--- frontend/app/src/main/java/com/example/frontend/MainActivity.java
+++ frontend/app/src/main/java/com/example/frontend/MainActivity.java
@@ -1,31 +1,64 @@
 package com.example.frontend;
 
 import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.widget.ImageView;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.squareup.picasso.Picasso;
+import com.example.frontend.fragments.HomeFragment;
+import com.example.frontend.fragments.MyBookFragment;
+import com.example.frontend.fragments.MyProfileFragment;
+import com.example.frontend.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.toolbar_custom);
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        frameLayout = findViewById(R.id.frame_layout);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                if (itemId == R.id.navHome) {
+                    loadFragment(new HomeFragment(), false);
+                } else if (itemId == R.id.navSearch) {
+                    loadFragment(new SearchFragment(), false);
+                } else if (itemId == R.id.navMyBook) {
+                    loadFragment(new MyBookFragment(), false);
+                } else {
+                    loadFragment(new MyProfileFragment(), false);
+                }
+                return true;
+            }
         });
-        ImageView img = findViewById(R.id.imgView);
-        Picasso.get().load("https://res.cloudinary.com/dwfo16yhs/image/upload/v1719050854/img_prn231/bwg2pcprjrb1graxbduh.jpg").into(img);
+
+
+    }
+
+    private void loadFragment(Fragment fragment, boolean isInit) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if (isInit) {
+            fragmentTransaction.add(R.id.frame_layout, fragment);
+
+        } else {
+            fragmentTransaction.replace(R.id.frame_layout, fragment);
+        }
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
--- frontend/app/src/main/java/com/example/frontend/fragments/MyBookFragment.java
+++ frontend/app/src/main/java/com/example/frontend/fragments/MyBookFragment.java
@@ -1,2 +1,19 @@
-package com.example.frontend.fragments;public class MyBookFragment {
+package com.example.frontend.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.frontend.R;
+
+public class MyBookFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.bookshelf_screen, container, false);
+    }
 }
